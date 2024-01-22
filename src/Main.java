0436694d8c20faccd49b82bc3db4c0521013119d
Adam <algolec1990@yahoo.com>
2024-01-22 01:11:21 -0500

    change code in Main as needed to accomidate changes made to Map class, and test the basic functionality of the class.

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        final int MAP_COL = 10;
-        final int MAP_ROW = 6;
+        final int MAP_COL = 6*2;
+        final int MAP_ROW = 4;
 
         HashMap<String,Map> maps = new HashMap<String,Map>();
-        Door aDoor = new Door("Enterance Gate",'D',0,0);
+        Door aDoor = new Door("Enterance Gate",'D',1,1);
         Door anotherDoor = new Door("House Door", 'H',MAP_ROW,MAP_COL);
         Map rpgMap = new Map(MAP_ROW,MAP_COL);
         maps.put("rpgMap",rpgMap);
 
         maps.get("rpgMap").placeEntity(aDoor, aDoor.getEntityColumnPosition(),aDoor.getEntityRowPosition());
 
-        maps.get("rpgMap").placeEntity(aDoor,aDoor.getEntityColumnPosition(), aDoor.getEntityRowPosition());
+//        maps.get("rpgMap").placeEntity(aDoor,aDoor.getEntityColumnPosition(), aDoor.getEntityRowPosition());
         maps.get("rpgMap").placeEntity(anotherDoor, anotherDoor.getEntityColumnPosition(), anotherDoor.getEntityRowPosition());
         maps.get("rpgMap").printMap();
 

2e497a53d763ec17bb1761eaa84161ff95754b9f
Adam <algolec1990@yahoo.com>
2024-01-22 01:09:49 -0500

    add out directory to .gitignore

--- src/map/Map.java
+++ src/map/Map.java
@@ -5,14 +5,12 @@ import entity.Entity;
 import java.util.*;
 
 public class Map {
+    private static final int BORDER_SIZE = 2;
     private char[][] grid;
     private int mapRows;
     private int mapColumns;
     private List<Entity> existingEntities;
-    final int TOP_ROW = 0;
-    final int LEFT_COLUMN = 0;
-    int RIGHT_COLUMN;
-    int BOTTOM_ROW;
+
     private final int GRID_BORDER_OFFSET = 2;
     private final int SPRITE_PLACEMENT_OFFSET = 1;
 //    public Map(int rows,int columns,List<Entity> entities){
@@ -25,55 +23,49 @@ public class Map {
 //        initializeMap();
 //    }
     public Map(int rows,int columns){
-        this.mapRows = rows + GRID_BORDER_OFFSET;
-        this.mapColumns = columns + GRID_BORDER_OFFSET;
-        this.RIGHT_COLUMN = mapColumns - 1;
-        this.BOTTOM_ROW = mapRows - 1;
+        this.mapRows = rows + BORDER_SIZE;
+        this.mapColumns = columns + BORDER_SIZE;
         this.grid = new char[mapRows][mapColumns];
         this.existingEntities = new ArrayList<Entity>();
         initializeMap();
     }
 
     private void initializeMap(){
-        for(int i = 0; i < mapRows; i++){
-            for(int j = 0; j < mapColumns; j++){
-                grid[i][j] = setGrid(i,j);
-            }
+        drawBorder();
+        drawGrid();
+    }
+    private void drawBorder(){
+        grid[0][0] = '┌';
+        grid[0][this.mapColumns - 1] = '┐';
+
+        for (int i = 1; i < this.mapColumns - 1; i++) {
+            grid[0][i] = '─';
+            grid[this.mapRows - 1][i] = '─';
+        }
+
+        grid[this.mapRows - 1][0] = '└';
+        grid[mapRows - 1][this.mapColumns - 1] = '┘';
+
+        for (int i = 1; i < mapRows - 1; i++) {
+            grid[i][0] = '│';
+            grid[i][this.mapColumns - 1] = '│';
         }
     }
-    private char setGrid(int row, int column){
 
-        if(row == TOP_ROW){
-            if(column == LEFT_COLUMN){
-                return '┌';
-            } else if (column == RIGHT_COLUMN){
-                return '┐';
-            } else {
-                return '─';
-            }
-        }
-        else if (row == BOTTOM_ROW){
-            if(column == LEFT_COLUMN){
-                return '└';
-            } else if (column == RIGHT_COLUMN){
-                return '┘';
-            } else {
-                return '─';
-            }
-        }
-        else {
-            if (column == LEFT_COLUMN || column == RIGHT_COLUMN) {
-                return '|';
-            } else {
-                return '.';
+    public void drawGrid(){
+        final int GRID_ROWS = this.mapRows - 1;
+        final int GRID_COLUMNS = this.mapColumns - 1;
+
+        for(int i = 1; i < GRID_ROWS; i ++){
+            for(int j = 1; j < GRID_COLUMNS; j++){
+                grid[i][j] = '.';
             }
         }
     }
     public void printMap(){
         for(int rowIterator = 0; rowIterator < mapRows; rowIterator++){
-            int nextRow = rowIterator + 1;
             for(int columnIterator = 0; columnIterator < mapColumns; columnIterator++){
-                System.out.print(grid[rowIterator][columnIterator] + " ");
+                System.out.print(grid[rowIterator][columnIterator]);
             }
             System.out.println();
         }
@@ -89,55 +81,21 @@ public class Map {
             System.out.println("Invalid position. Select another space.");
         }
     }
-
-    private void placeSpriteOnGrid(Entity entity) {
-        int entityRow;
-        int entityColumn;
-        System.out.println(entity);
-        if(entity.getEntityRowPosition() + GRID_BORDER_OFFSET == this.mapColumns){
-            entityColumn = entity.getEntityRowPosition();
-        } else {
-            entityColumn = entity.getEntityRowPosition() + SPRITE_PLACEMENT_OFFSET;
-        }
-        if(entity.getEntityColumnPosition() + GRID_BORDER_OFFSET == this.mapRows){
-            entityRow = entity.getEntityColumnPosition();
-        } else {
-            entityRow = entity.getEntityColumnPosition() + SPRITE_PLACEMENT_OFFSET;
-        }
-        grid[entityRow][entityColumn] = entity.getEntitySprite();
+    public void forcePlaceEntity(Entity entity, int d, int r){
+        placeSpriteOnGrid(entity);
     }
 
-    private boolean canPlaceEntityOnMap(Entity entityToPlace,int desiredXPosition,int desiredYPosition) {
-        // Check if the position is within the bounds of the map, and collision.
+    private void placeSpriteOnGrid(Entity entity) {
+        grid[entity.getEntityColumnPosition()][entity.getEntityRowPosition()] = entity.getEntitySprite();
+    }
 
-        //check the position of each entity on the map.
-        //If the entity we are trying to place has the same position coordinates as an entity we are trying to place on the map,
-        // check if the existing entity on the map can collide with the desired entity.
-
-        //if existing entity is unable to collide, the position is invalid.
-        int desiredXPositionRelativeToLeftColumn = desiredXPosition + SPRITE_PLACEMENT_OFFSET;
-        int desiredXPositionRelativeToRightColumn = desiredXPosition - (2 * SPRITE_PLACEMENT_OFFSET);
-
-        int desiredYPositionRelativeToTopRow = desiredYPosition + SPRITE_PLACEMENT_OFFSET;
-        int desiredYPositionRelativeToBottomRow = desiredYPosition - (2 * SPRITE_PLACEMENT_OFFSET);
-
-        if(desiredXPositionRelativeToLeftColumn <= this.LEFT_COLUMN || desiredXPositionRelativeToRightColumn  >= this.RIGHT_COLUMN
-                || desiredYPositionRelativeToTopRow <= this.TOP_ROW || desiredYPositionRelativeToBottomRow >= this.BOTTOM_ROW){
+    private boolean canPlaceEntityOnMap(Entity entityToPlace,int desiredRowPosition,int desiredColumnPosition) {
+        final int MAX_GRID_ROW = this.mapRows - 1;
+        final int MAX_GRID_COLUMN = this.mapColumns - 1;
+        if(desiredRowPosition < 1 || desiredRowPosition > MAX_GRID_ROW - 1){
             return false;
-        }
-
-        if(this.existingEntities != null){
-            for(Entity existingEntity : existingEntities){
-                if(existingEntity.entityHasDesiredPositionOfAnotherEntity(desiredXPosition,desiredYPosition)){
-                    if(existingEntity.canEntityCollide()){
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-            }
-        } else {
-
+        } else if (desiredColumnPosition < 1 || desiredColumnPosition > MAX_GRID_COLUMN - 1){
+            return false;
         }
         return true;
     }

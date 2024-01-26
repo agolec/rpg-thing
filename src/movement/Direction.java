31c6b055965de1def1cb93f4dbeee35d4174af90
Adam <algolec1990@yahoo.com>
2024-01-25 21:15:49 -0500

    set up the start of the direction enum class.

--- src/movement/Direction.java
+++ src/movement/Direction.java
@@ -1,2 +1,19 @@
-package movement;public enum Direction {
+package movement;
+
+public enum Direction {
+    UP, DOWN, LEFT, RIGHT;
+    public static Direction fromString(String directionString){
+        switch(directionString.toUpperCase()){
+            case "UP":
+                return UP;
+            case "DOWN":
+                return DOWN;
+            case "LEFT":
+                return LEFT;
+            case "RIGHT":
+                return RIGHT;
+            default:
+                throw new IllegalArgumentException("Invalid direction string entered: \'" + directionString + "\'");
+        }
+    }
 }
